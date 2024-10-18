20d343ee3097ed8297f0a6a623ee02bcf7250c11
Tan Loc Nguyen <talu.dev@gmail.com>
2024-10-18 21:41:02 +1100

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/sync-draft/",
+
+  // server: {
+  //   port: 3000
+  // },
+
   plugins: [
     react(),
     tsconfigPaths(),
+    nodePolyfills(),
     wasm(),
     topLevelAwait()
-  ]
+  ],
+  worker: {
+    format: "es",
+    plugins: () => [wasm()],
+  },
 })
