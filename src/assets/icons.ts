c1a21a5ea3e22c7bc05bc43fa75282a191bac815
Tan Loc Nguyen <talu.dev@gmail.com>
2024-10-20 02:01:16 +1100

    add icons

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,18 +1,17 @@
-import React from 'react';
+import DocumentIcon from '@/assets/icons/DocumentIcon';
+import PlusIcon from '@/assets/icons/PlusIcon';
+import SearchIcon from '@/assets/icons/SearchIcon';
+import ShareIcon from '@/assets/icons/ShareIcon';
+import ToggleSidebarIcon from '@/assets/icons/ToggleSidebarIcon';
+import GroupIcon from '@/assets/icons/GroupIcon';
+import HomeIcon from '@/assets/icons/HomeIcon';
 
-export const DocumentIcon = React.lazy(() => import('@/assets/icons/DocumentIcon'));
-export const PlusIcon = React.lazy(() => import('@/assets/icons/PlusIcon'));
-export const SearchIcon = React.lazy(() => import('@/assets/icons/SearchIcon'));
-export const ShareIcon = React.lazy(() => import('@/assets/icons/ShareIcon'));
-export const ToggleSidebarIcon = React.lazy(() => import('@/assets/icons/ToggleSidebarIcon'));
-
-
-const icons = {
+export {
   DocumentIcon,
   PlusIcon,
   SearchIcon,
   ShareIcon,
-  ToggleSidebarIcon
+  ToggleSidebarIcon,
+  GroupIcon,
+  HomeIcon
 };
-
-export default icons;
