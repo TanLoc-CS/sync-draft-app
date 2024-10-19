51110e366fbf79448e7251f2c97552317dd56a05
Tan Loc Nguyen <talu.dev@gmail.com>
2024-10-19 16:31:35 +1100

    responsive screen container

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+      screens: {
+        sm: '100%',  // Full width on small screens
+        md: '600px', // Custom width for medium screens
+        lg: '768px', // Custom width for large screens
+        xl: '1280px',
+        '2xl': '1536px', // Custom width for extra large screens
+        '3xl': '1920px', // Custom width for 2xl screens
+      }
+    },
     extend: {},
   },
   plugins: [],
