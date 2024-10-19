034c92114416831b72b6bf290474aaa703915eed
Tan Loc Nguyen <talu.dev@gmail.com>
2024-10-20 02:01:39 +1100

    reconfig screen resolution

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,16 @@ export default {
   theme: {
     container: {
       center: true,
-      padding: '16px',
       screens: {
         sm: '100%',  // Full width on small screens
         md: '600px', // Custom width for medium screens
         lg: '768px', // Custom width for large screens
-        xl: '1280px',
-        '2xl': '1536px', // Custom width for extra large screens
-        '3xl': '1920px', // Custom width for 2xl screens
+        xl: '962px',
+        '2xl': '1024px',
+        '3xl': '1280px',
+        '4xl': '1536px', // Custom width for extra large screens
+        '5xl': '1600px',
+        '6xl': '1920px', // Custom width for 2xl screens
       }
     },
     extend: {},
