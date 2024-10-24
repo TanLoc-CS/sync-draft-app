8e8e778856a425123bf8208ad27c9a9af0846b13
Tan Loc Nguyen <talu.dev@gmail.com>
2024-10-25 00:54:36 +1100

    add UserBubble

---
+++ src/components/UserBubble.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export interface UserBubbleProps {
+  
+}
+
+export const UserBubble = () => {
+  return (
+    <div className='w-[40px] h-[40px] ml-3 bg-amber-200 flex flex-col justify-center items-center rounded-full'>
+      TLN
+    </div>
+  )
+}
