a6b7cf159cb9a4efce79684b52ba68cdfbfc5263
Tan Loc Nguyen <talu.dev@gmail.com>
2024-10-25 00:54:48 +1100

    modify Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,42 +1,54 @@
 import React from 'react';
 
 export enum ButtonSize {
-  sm = 'w-[60px]',
+  sm = 'w-[64px]',
   md = 'w-[120px]',
-  lg = 'w-[180px]'
+  lg = 'w-[204px]'
+}
+
+export enum ButtonColor {
+  white= 'bg-white text-black hover:bg-gray-200 border',
+  black= 'bg-black text-white hover:bg-gray-800'
 }
 
 export interface ButtonProps {
   label?: string,
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void,
-  size: ButtonSize,
+  color?: ButtonColor,
+  size?: ButtonSize,
   icon?: React.ReactElement, // svg
   disabled?: boolean,
   loading?: boolean,
-  submit?: boolean
+  submit?: boolean,
+  className?: string
 }
 
 const Button : React.FC<ButtonProps> = ({
   label,
   icon,
   size=ButtonSize.md,
+  color=ButtonColor.white,
   onClick,
   disabled = false,
   loading = false,
-  submit = false
+  submit = false,
+  className=''
 }) => {
+
   return (
     <button
       disabled={disabled}
       type={submit ? 'submit' : 'button'}
       onClick={onClick}
       className={
-        `${size} h-[48px] p-4 rounded-lg bg-black text-white hover:bg-gray-800 ${loading ? 'flex flex-row justify-center items-center' : 'flex flex-row justify-between items-center'}
+        `${className} ${size} h-[36px] p-4 rounded-lg ${color} ${loading ? 'flex flex-row justify-center items-center' : 'flex flex-row justify-start items-center'}
       `}
     >
       {loading && <div>Loading...</div>}
       {!loading && icon && icon}
-      {!loading && label && label}
+      <p className='ml-3'>
+        {!loading && label && label}
+      </p>
     </button>
   );
 }
