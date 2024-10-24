9099e772474cb249545055ca16f0969f1fd609df
Tan Loc Nguyen <talu.dev@gmail.com>
2024-10-25 00:56:12 +1100

    add Draft component

---
+++ src/components/Draft.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Button from '@/components/Button';
+
+export interface DraftProps {
+  isMerged: boolean,
+  draftId: string,
+  draftTitle: string,
+  updatedAt: string,
+} 
+
+const Draft: React.FC<DraftProps> = ({
+  isMerged,
+  draftId,
+  draftTitle,
+  updatedAt
+}) => {
+
+  
+  return (
+    <div 
+      className='w-full h-[60px] mb-2 p-2 bg-white rounded-lg hover:bg-gray-200 flex flex-col justify-between items-start
+    '>
+      <p>
+        {draftTitle}
+      </p>
+      <p className='text-gray-500 font-normal text-[13px]'>
+        {isMerged? `Last update: ${updatedAt}` : `Merged by: ${updatedAt}`}
+      </p>
+    </div>
+  )
+};
+
+export default Draft;
